a4c02be79cdff45a47ade77a290fe744eecfc278
moriarty83 <moriarty83@gmail.com>
2021-11-23 15:42:46 -0500

    footer update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,18 +18,18 @@ const handleExit = ()=>{
 }
     return(
         <div className="foot-container" onMouseLeave={handleExit}>
-        <div className={infoOn ? "about-div " : "about-div hidden"}>
-            <h1>About Christopher Moriarty</h1>
-            <p>I’m a Software Engineer with a background in Business Management and Customer Experience. Thanks to my extensive experience in e-commerce and nonprofit organizations, I approach each project with an empathetic and user-focused perspective. By coupling analytical skills with a love of learning, I am able to solve complex and systematic problems in my pursuit of building unique and empowering experiences that inspire joy and wonder.
-<br /><br />
-Technical Skills:<br />
-JavaScript | TypeScript | Express | React | HTML | CSS | EJS | JSX | C# | Python | Django | Flask | Git | GitHub | MySQL | Unity 3D | PhotoShop, Machine Learning, Markdown, Contentful CMS</p>
-        </div>
-        <footer>
-            <p onMouseEnter={handleEnter} >Page by Christopher Moriarty - Hover here to learn about the developer.</p>
-            <p className="version">DRAW Version 0.1</p>
-            
-        </footer>
+            <div className={infoOn ? "about-div " : "about-div hidden"}>
+                <h1>About Christopher Moriarty</h1>
+                <p>I’m a Software Engineer with a background in Business Management and Customer Experience. Thanks to my extensive experience in e-commerce and nonprofit organizations, I approach each project with an empathetic and user-focused perspective. By coupling analytical skills with a love of learning, I am able to solve complex and systematic problems in my pursuit of building unique and empowering experiences that inspire joy and wonder.
+                <br /><br />
+                Technical Skills:<br />
+                JavaScript | TypeScript | Express | React | HTML | CSS | EJS | JSX | C# | Python | Django | Flask | Git | GitHub | MySQL | Unity 3D | PhotoShop, Machine Learning, Markdown, Contentful CMS</p>
+            </div>
+            <footer>
+                <p onMouseEnter={handleEnter} >Page by Christopher Moriarty <br />Hover here to learn about the developer.</p>
+                <p className="version">DRAW Version 0.1</p>
+                
+            </footer>
         </div>
         
     )
