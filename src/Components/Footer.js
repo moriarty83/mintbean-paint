33f42f84f2386dabbd37546383bfc81080f7fc21
moriarty83 <moriarty83@gmail.com>
2021-11-23 21:59:18 -0500

    skills update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,7 +23,7 @@ const handleExit = ()=>{
                 <p>I’m a Software Engineer with a background in Business Management and Customer Experience. Thanks to my extensive experience in e-commerce and nonprofit organizations, I approach each project with an empathetic and user-focused perspective. By coupling analytical skills with a love of learning, I am able to solve complex and systematic problems in my pursuit of building unique and empowering experiences that inspire joy and wonder.
                 <br /><br />
                 Technical Skills:<br />
-                JavaScript | TypeScript | Express | React | HTML | CSS | EJS | JSX | C# | Python | Django | Flask | Git | GitHub | MySQL | Unity 3D | PhotoShop, Machine Learning, Markdown, Contentful CMS</p>
+                JavaScript | TypeScript | Express | React | HTML | CSS | EJS | JSX | C# | Python | Django | Flask | Git | GitHub | MySQL | Mongo DB | Mongoose | Unity 3D | PhotoShop | Machine Learning | Markdown | Contentful CMS</p>
             </div>
             <footer>
                 <p onMouseEnter={handleEnter} >Page by Christopher Moriarty <br />Hover here to learn about the developer.</p>
